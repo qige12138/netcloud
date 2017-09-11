42c7e19eb74eb38278dd3f63f919eae204d6c299
yrj <517229333@qq.com>
2017-09-11 16:07:01 +0800

    1

--- netcloud/src/main.js
+++ netcloud/src/main.js
@@ -2,7 +2,7 @@
 
 import 'babel-polyfill'
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import router from './index'
 
 
@@ -10,8 +10,6 @@ Vue.config.productionTip = false
 
 
 new Vue({
-  el: '#app',
   router,
-  template: '<App/>',
-  components: {App}
-})
+  render: h => h(App)
+}).$mount('#app')
