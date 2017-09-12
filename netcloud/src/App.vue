64c939610dddd2c81e9b4cd4b1f249c388f374e6
yrj <517229333@qq.com>
2017-09-12 14:09:39 +0800

    1

---
+++ netcloud/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+	<div id="app">
+		<my-header></my-header>
+	</div>
+</template>
+
+<script type="text/ecmascript-6">
+	import myHeader from './components/myheader/myheader.vue'
+
+	
+	export default {
+		components:{
+			myHeader
+		}
+	}
+</script>
+
+<style scoped lang="stylus" rel-"stylesheet/stylus">
+	// @import 
+</style>
---
+++ netcloud/src/components/myheader/myheader.vue
@@ -0,0 +1,15 @@
+<template>
+	<header>
+		头部
+	</header>
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
+
+<style lang="stylus">
+	
+</style>
